542870e8f3638b9aacae861ed1f7053052718f56
Sylvain CAU <ashdevfr@gmail.com>
2017-01-05 22:47:49 -0800

    :shirt: Refacto

--- src/app/components/commons/ToggleSwitch/ToggleSwitch.js
+++ src/app/components/commons/ToggleSwitch/ToggleSwitch.js
@@ -3,17 +3,19 @@ import React from 'react';
 import './ToggleSwitch.scss';
 
 export default class ToggleSwitch extends React.Component {
-  handleClick () {
-    this.props.action && this.props.action(this.refs.input.checked);
-  }
+  handleClick = e => {
+    this.props.action && this.props.action(e.target.checked);
+  };
+
   render() {
     const classes = 'toggle-switch' + (this.props.className ? ` ${this.props.className}` : '');
+    const id = `switch-${Math.random().toString(36).substring(7)}`;
     return (
       <div className={classes}>
-        <input id="cmn-toggle-1" className="cmn-toggle cmn-toggle-round"
-        type="checkbox" ref="input"
-        onClick={(e) => this.handleClick()}/>
-        <label htmlFor="cmn-toggle-1"/>
+        <input id={id} className="cmn-toggle cmn-toggle-round"
+        type="checkbox"
+        onClick={this.handleClick}/>
+        <label htmlFor={id}/>
       </div>
     );
   }
